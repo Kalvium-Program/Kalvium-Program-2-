60390e4687fc8137d999c371ec314298197069ca
Kalvium <109285740+Kalvium-Program@users.noreply.github.com>
2024-11-19 06:36:18 +0530

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,7 @@
+// Donot edit the below code
+
+let button = document.getElementById("button");
+var numberInTheShape = 1;
+var oldValue = 0;
+
+// Write the functions required to get the desired output
